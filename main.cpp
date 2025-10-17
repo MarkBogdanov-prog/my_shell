2fe9b85202c0d9b7b4e3ef7a95e9443e7caa5570
MarkBogdanov-prog <markbogdanovjob@gmail.com>
2025-10-17 19:05:00 +0300

    zad3: Add \q command to exit the shell loop

--- main.cpp
+++ main.cpp
@@ -6,7 +6,10 @@ using namespace std;
 int main() {
     string input;
     while (getline(cin, input)) {
+        if (input == "\\q") 
+            break;
         cout << input << endl;
     }
     return 0;
 }
+

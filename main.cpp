79a17c816a8e198747ff5beaea3cae7bb738f17c
MarkBogdanov-prog <markbogdanovjob@gmail.com>
2025-10-21 11:32:00 +0300

    zad4: Add history and persist to ~/.kubsh_history

--- main.cpp
+++ main.cpp
@@ -1,15 +1,22 @@
 #include <iostream>
+#include <fstream>
 #include <string>
 
 using namespace std;
 
 int main() {
+    string hist = "kubsh_history.txt";
+    ofstream F(hist, ios::app);
+
     string input;
     while (getline(cin, input)) {
+        F << '$' << input << '\n';  
+        F.flush();
+        
         if (input == "\\q") 
             break;
+        
         cout << input << endl;
     }
     return 0;
 }
-

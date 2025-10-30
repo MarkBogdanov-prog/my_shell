8dc16f0855ea8fc8463a07098c3c47785f0d85df
MarkBogdanov-prog <markbogdanovjob@gmail.com>
2025-10-30 09:48:00 +0300

    zad6: Add basic command existence check

--- main.cpp
+++ main.cpp
@@ -1,30 +1,44 @@
+
 #include <iostream>
 #include <fstream>
 #include <string>
 
 using namespace std;
 
+void handle_debug(string input) {
+    input = input.substr(5);
+    while (!input.empty() && input[0] == ' ') {
+        input.erase(0, 1);
+    }
+    cout << input << '\n';
+}
+
 int main() {
     string hist = "kubsh_history.txt";
     ofstream F(hist, ios::app);
 
+    cerr << "$ ";
+
     string input;
     while (getline(cin, input)) {
         F << '$' << input << '\n';  
-        F.flush();
+        F.flush(); 
         
         if (input == "\\q") 
             break;
         
-        if (input.find("echo") == 0) {
-            string echo_text = input.substr(4);
-            while (!echo_text.empty() && echo_text[0] == ' ') {
-                echo_text.erase(0, 1);
-            }
-            cout << echo_text << '\n';
-        } else {
-            cout << input << endl;
+        if (input.empty()) {
+            cerr << "$ ";
+            continue;
         }
+        
+        if (input.find("debug") == 0) {
+            handle_debug(input);
+        } else {
+            cout << "Unknown command: " << input << '\n';
+        }
+
+        cerr << "$ ";
     }
     return 0;
 }

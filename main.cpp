cdd173f0c9852d62fbe6aff771e8e1474cc1bc7c
MarkBogdanov-prog <markbogdanovjob@gmail.com>
2025-11-03 13:21:00 +0300

    zad7: Add \e builtin to print environment variables

--- main.cpp
+++ main.cpp
@@ -1,7 +1,9 @@
-
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <vector>
+#include <sstream>
+#include <cstdlib>
 
 using namespace std;
 
@@ -11,6 +13,35 @@ void handle_debug(string input) {
         input.erase(0, 1);
     }
     cout << input << '\n';
+}
+
+void handle_env(string input) {
+    size_t pos = input.find("\\e") + 3;
+    
+    if (pos < input.length()) {
+        string var_name = input.substr(pos);
+        
+        if (!var_name.empty() && var_name[0] == '$') 
+            var_name = var_name.substr(1);
+        
+        const char* env_value = getenv(var_name.c_str());
+        if (env_value != nullptr) {
+            string value = env_value;
+            size_t start = 0;
+            size_t end = value.find(':');
+            
+            while (end != string::npos) {
+                cout << value.substr(start, end - start) << '\n';
+                start = end + 1;
+                end = value.find(':', start);
+            }
+            cout << value.substr(start) << '\n';
+        } else {
+            cout << "Environment variable '" << var_name << "' not found" << '\n';
+        }
+    } else {
+        cout << "Usage: \\e $VARIABLE" << '\n';
+    }
 }
 
 int main() {
@@ -34,6 +65,8 @@ int main() {
         
         if (input.find("debug") == 0) {
             handle_debug(input);
+        } else if (input.find("\\e") == 0) {
+            handle_env(input);
         } else {
             cout << "Unknown command: " << input << '\n';
         }
@@ -42,3 +75,4 @@ int main() {
     }
     return 0;
 }
+
